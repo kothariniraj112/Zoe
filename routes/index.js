8fa3bd008816c224190a96bcf92d2474a283e1e0
Niraj <kothariniraj112@gmail.com>
2019-01-01 14:42:36 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,11 @@ var request = require('request');
 const cheerio = require('cheerio')
 const $ = cheerio.load('<h2 class="title">Hello world</h2>')
 
-
+// https://docs.google.com/forms/d/e/1FAIpQLSf_hi_8FmmRDatJ_y7X3GXIzO8QD3aDgpqFcvkkrqWR1rRXSA/viewform?usp=sf_link
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  request('https://docs.google.com/forms/d/e/1FAIpQLSf_hi_8FmmRDatJ_y7X3GXIzO8QD3aDgpqFcvkkrqWR1rRXSA/viewform?usp=sf_link', function (error, response, body) {
+  request('https://goo.gl/forms/xJjb5uIvqBDGUv4P2', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     // res.send('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     // res.send({body:body}); // Print the HTML for the Google homepage.
